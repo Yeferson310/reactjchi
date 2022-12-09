07c90364e8b3de2145104d9c13c0b5401130ee78
Yeferson310 <keivinaguiar1411@gmail.com>
2022-12-09 08:43:35 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import cards from './cards/cards';
+import { pokemon } from './database/db';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> {pokemon.map((item)=>(
+      <cards id = {item.id} img = {item.img} name = {item.name}/>
+    ))} </div>
   );
 }
 
---
+++ src/cards/cards.jsx
@@ -0,0 +1,10 @@
+const cards = (props) => {
+    return 
+    <div>
+        <span> #{props.id} </span>
+        <img src= {props.img} />
+        <h1>{props.name} </h1>
+    </div>
+    } 
+
+    export default cards 
